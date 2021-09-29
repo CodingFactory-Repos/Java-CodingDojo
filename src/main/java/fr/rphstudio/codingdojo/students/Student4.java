6468ac4c76f986608ef690f865c9b6e21ac8d3da
Loule | Louis <35641311+Loule95450@users.noreply.github.com>
2021-09-29 17:07:16 +0200

    🔋 - Update Battery Function

--- src/main/java/fr/rphstudio/codingdojo/students/Student4.java
+++ src/main/java/fr/rphstudio/codingdojo/students/Student4.java
@@ -5,7 +5,6 @@
  */
 package fr.rphstudio.codingdojo.students;
 
-
 import fr.rphstudio.codingdojo.game.Pod;
 import fr.rphstudio.codingdojo.game.PodPlugIn;
 
@@ -16,22 +15,33 @@ import java.lang.reflect.Array;
  * @author Romuald GRIGNON
  */
 public class Student4 extends PodPlugIn {
-    public Student4(Pod p){
+    public Student4(Pod p) {
         super(p);
     }
 
-    //-------------------------------------------------------
+    // -------------------------------------------------------
     // DECLARE YOUR OWN VARIABLES AND FUNCTIONS HERE
     //
 
+    public static boolean needForRecharge = false;
+
     // Create updateChargingMode variable to check if battery
-    public static boolean updateChargingMode(float a){
-        return a <= 30;
+    public boolean getUpdateChargingMode(float a) {
+        if(a <= 30 && !needForRecharge){
+            needForRecharge = true;
+            return true;
+        } else if(a >= 90 && needForRecharge){
+            needForRecharge = false;
+            return false;
+        } else if(needForRecharge){
+            return true;
+        } else {
+            return false;
+        }
     }
 
-
     public int checkpointCharging(int a) {
-        for(int i = 1; i <= a; i++) {
+        for (int i = 1; i <= a; i++) {
             if (isCheckPointCharging(i)) {
                 return i;
             }
@@ -39,38 +49,40 @@ public class Student4 extends PodPlugIn {
         return 0;
     }
 
+
     //
     // END OF VARIABLES/FUNCTIONS AREA
-    //-------------------------------------------------------
+    // -------------------------------------------------------
 
     @Override
-    public void process(int delta)
-    {
-        //-------------------------------------------------------
+    public void process(int delta) {
+        // -------------------------------------------------------
         // WRITE YOUR OWN CODE HERE
         //
 
-        setPlayerName("Wallah tu es trop beau");
+        setPlayerName("TDBC");
         selectShip(1);
-        setPlayerColor(247, 143, 179,255);
+        setPlayerColor(247, 143, 179, 255);
         // getFirstChargingCheckPointIndex();
 
-        //moveAndRecharge(1f, 0, 100);
+        // moveAndRecharge(1f, 0, 100);
 
         // Check if the battery is below 30%.
         // If battery < 30% go to Charging CkeckPoint
         // Otherwise continue as normal
-        if(updateChargingMode(getShipBatteryLevel())) {
-            turnTowardPosition(getCheckPointX(checkpointCharging(getNbRaceCheckPoints())),getCheckPointY(checkpointCharging(getNbRaceCheckPoints())));
-            accelerateOrBrake(0.5f);
+        if (getUpdateChargingMode(getShipBatteryLevel())) {
+            turnTowardPosition(getCheckPointX(checkpointCharging(getNbRaceCheckPoints())), getCheckPointY(checkpointCharging(getNbRaceCheckPoints())));
+            accelerateOrBrake(0.3f);
         } else {
-            turnTowardPosition(getCheckPointX(getNextCheckPointIndex()),getCheckPointY(getNextCheckPointIndex()));
-            accelerateOrBrake (1f);
+            turnTowardPosition(getCheckPointX(getNextCheckPointIndex()), getCheckPointY(getNextCheckPointIndex()));
+            accelerateOrBrake(1f);
         }
+
+        System.out.println(getShipSpeed());
 
         //
         // END OF CODE AREA
-        //-------------------------------------------------------
+        // -------------------------------------------------------
     }
 
 }
